453740d46c1e842365c251c6e454a2b191296ae2
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-05 23:06:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   element.unshift();
-  return addElementToBeginningOfArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element)
