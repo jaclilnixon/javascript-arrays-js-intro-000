b398b34170237e43740d8ccbe46aed63065d23fc
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-05 23:25:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,10 +9,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 }
 
-function addElementToEndOfArray () {
+function addElementToEndOfArray(array, element) {
 
 }
 
-function destructivelyAddElementToEndOfArray () {
+function destructivelyAddElementToEndOfArray(array, element) {
   
 }
